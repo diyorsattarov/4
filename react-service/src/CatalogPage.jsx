044016deddb23b30c6a33e351262a4a34151b6a2
diyooor <diyorsattarov7@gmail.com>
2023-12-28 20:16:01 -0600

    Added new feature pagination, introduced Bug #1

--- react-service/src/CatalogPage.jsx
+++ react-service/src/CatalogPage.jsx
@@ -2,24 +2,36 @@ import React, { useState, useEffect } from 'react';
 
 function Catalog() {
   const [products, setProducts] = useState([]);
-
+  const [currentPage, setCurrentPage] = useState(1);
+  const productsPerPage = 10; // Fixed number of products per page
+  
   useEffect(() => {
-    fetch('http://localhost:5000/get_all_products')
-      .then((response) => {
+    fetch(`http://localhost:5000/get_all_products?page=${currentPage}`)
+      .then(response => {
         if (!response.ok) {
           throw new Error('Failed to fetch products');
         }
         return response.json();
       })
-      .then((data) => {
+      .then(data => {
         console.log('Received products:', data);
         setProducts(data);
       })
-      .catch((error) => {
+      .catch(error => {
         console.error('Error:', error);
       });
-  }, []); // Empty dependency array to fetch only once when the component mounts
+  }, [currentPage]); // Dependency array includes currentPage
+  
 
+  const handleNextPage = () => {
+    setCurrentPage(currentPage + 1);
+  };
+  
+  const handlePreviousPage = () => {
+    setCurrentPage(currentPage - 1);
+  };
+  
+  // Inside the return statement of the Catalog component
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
       <h1>Product Catalog</h1>
@@ -30,8 +42,15 @@ function Catalog() {
           <p><strong>Description:</strong> {product.description}</p>
         </div>
       ))}
+  
+      <div>
+        <button onClick={handlePreviousPage} disabled={currentPage === 1}>Previous</button>
+        <span>Page {currentPage}</span>
+        <button onClick={handleNextPage}>Next</button> {/* Add logic to disable when on last page */}
+      </div>
     </div>
   );
+  
 }
 
 export default Catalog;
